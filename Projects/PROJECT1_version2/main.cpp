5363dbc86b96bc20f8ac77e0c8ced747f4cb3ee5
CrsRamos <cramos32@student.rccd.edu>
2020-07-18 17:41:42 -0700

    Signed-off-by: CrsRamos <cramos32@student.rccd.edu>

--- Projects/PROJECT1_version2/main.cpp
+++ Projects/PROJECT1_version2/main.cpp
@@ -1,13 +1,18 @@
 /* 
  * File:   main.cpp
- * Author: Chris Ramos
+ * Author: Created on July 10, 2020, 3:37 PM
  * Created on July 18, 2020, 4:38 PM
- * Purpose:  Template which is to be copied for all future
- *           Homework, Labs, Projects, Test, etc...
+ * Purpose: Craps version 2 testing
+ *           
  */
 
 //System Libraries
-#include <iostream>  //I/O Library
+
+#include <iostream>     //I/O Library
+#include <cstdlib>      // library for rand and srand
+#include <ctime>        // library for the time function
+#include <iomanip>      // library for formatting
+#include <string>       // string library
 using namespace std;
 
 //User Libraries
@@ -17,23 +22,57 @@ using namespace std;
 
 //Function Prototypes
 
-
 int main(int argc, char** argv) { //Execution of Code Begins Here
-    
+
     //Set the random number seed here
     
     //Declare all variables for this function
-    
-    //Initialize all known variables
-    
-    //Process Inputs to Outputs -> Mapping Process
+     int dice1, dice2, sum, point, newSum, rollNum;
+     char start; // start game
+     string result; 
+     float wager;
+     
+     //Process Inputs to Outputs -> Mapping Process
     //Maps known values to the unknown objectives
-    
-    //Display the Inputs/Outputs
 
-    //Clean up the code, close files, deallocate memory, etc....
-    //Exit stage right
+    cout << " \t\tRULES OF CRAPS" << endl;
+    cout << "*********************************************";  
+    cout << endl;
+    
+    // display the rules
+    cout << setw(30) << "On the first roll" << endl;
+    cout << "7 or 11" << setw(34) << "You win" <<endl;
+    cout << "2, 3, 12" << setw(33) << "You lose"<<endl;
+    cout << "4, 5, 6, 8, 9, 10" << setw(24) << "Roll again"<<endl;
+    cout << endl;
+    cout << setw(30) << "On the second roll" << endl;
+    cout << setw(43) << "If you rolled a number from the roll \n";
+    cout << setw(43) << "again row get it again before a 7 to win.";
+    cout << endl;
+    cout << endl;
+    
+    // start the game
+    cout <<"Enter a S to start the game ";
+    cin >> start;
+    
+    // validate
+    while ( start != 'S' && start != 's') {
+        cout <<"You must enter an S to start ";
+        cin >> start;
+        cout << endl;
+    }// end of while loop for validation
+    
+   
+    
+    // start the rolling
+   cout << "Enter the first dice number: ";
+   cin >> dice1;
+    
+    cout << "Enter the second dice number: ";
+    cin >> dice2;
+    cout << endl;
+    sum = dice1 + dice2;
+
     return 0;
 }
 
- 
