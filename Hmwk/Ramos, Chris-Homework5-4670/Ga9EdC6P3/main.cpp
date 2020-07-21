7297d779bc46d62617c01f39a2f67838122781c2
CrsRamos <cramos32@student.rccd.edu>
2020-07-21 15:43:47 -0700

    Signed-off-by: CrsRamos <cramos32@student.rccd.edu>

---
+++ Hmwk/Ramos, Chris-Homework5-4670/Ga9EdC6P3/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: Chris Ramos
+ * Created on July 21, 2020, 3:17 PM
+ * Purpose:  Template which is to be copied for all future
+ *           Homework, Labs, Projects, Test, etc...
+ */
+
+//System Libraries
+#include <iostream>  //I/O Library
+using namespace std;
+
+//User Libraries
+
+//Global Constants Only
+//Well known Science, Mathematical and Laboratory Constants
+
+//Function Prototypes
+float getSales(string);
+void findHighest(float, float, float, float);
+
+int main(int argc, char** argv) { //Execution of Code Begins Here
+  
+    //Declare all variables for this function
+    string neDiv,   // Northeast division
+           seDiv,   // Southeast division
+           nwDiv,   // Northest division
+           swDiv;   // Southwest division
+           
+    float neSales
+    
+    
+    //Initialize all known variables
+    
+    //Process Inputs to Outputs -> Mapping Process
+    //Maps known values to the unknown objectives
+    
+    //Display the Inputs/Outputs
+
+    //Clean up the code, close files, deallocate memory, etc....
+    //Exit stage right
+    
+    
+    
+    
+    return 0;
+}// end of main
+
+// function definitions
+
